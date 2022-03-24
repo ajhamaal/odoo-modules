fb3a177236f4f334ee8fd85c55721511155476bb
ajhamaal <ajhamaal@gullysound.net>
2022-03-25 00:49:29 +0100

    account_chart

---
+++ Odoo-12-Kenyan-Accounting-System-master/README.rst
@@ -0,0 +1,21 @@
+Description
+===============================
+
+This module Provides Basic Kenyan Chart of Accounts localisation necessary to run Odoo in Kenya:
+* Defines templates for sale and purchase VAT
+* Defines tax templates
+
+Installation
+============
+
+Just Install
+
+Credits
+=======
+Contributors
+------------
+* Dishon Kadoh <dishon.kadoh@gmail.com>
+
+Maintainer
+----------
+This module is maintained by Rottal Solutions Limited <https://rottalsolutions.com>
---
+++ Odoo-12-Kenyan-Accounting-System-master/l10n_kenya/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+
+# Copyright (C) 2019 Dishon Kadoh (<dishon.kadoh@gmail.com>).
+
+{
+    'name': 'Odoo15 Kenya - Accounting',
+    'version': '15.0.0.0',
+    'category': 'Localization',
+    'license': 'AGPL-3',
+    'description': """
+        Basic Kenyan Chart of Accounts localisation necessary to run Odoo in Kenya:
+        ================================================================================
+        * A generic chart of accounts
+        * Defines templates for sale and purchase VAT
+        * Defines tax templates
+   """,
+    'author': 'Dishon Kadoh',
+    'website': 'https://rottalsolutions.com',
+    'depends': ['account', 'base_vat'],
+    'data': [
+        'data/account.account.tag.csv',
+        'data/account.tax.group.csv',
+        'data/account_chart_template_data.xml',
+        'data/account.account.template.csv',
+        'data/account_tax_template_data.xml',
+        'data/account_chart_template_post_data.xml',
+        'data/account_chart_template_configure_data.xml',
+    ],
+}
